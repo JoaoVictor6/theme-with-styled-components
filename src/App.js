c4f3b2e41bffdb5f1e733f1411a750eb904492ae
Joao Victor <santosjoao.dev@gmail.com>
2021-09-02 20:20:27 -0300

    theme provide example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from 'styled-components';
+import Home from './pages/Home';
+import useCurrentTheme, { ThemeContext } from './themeContext';
+
+const themeValues = {
+  Light: {
+    backgroundColor: '#fefefe'
+  },
+  Dark: {
+    backgroundColor: '#1C2128'
+  }
+}
 
 function App() {
+  const {currentTheme, setCurrentTheme} = useCurrentTheme()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeValues[currentTheme]}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/Home/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  background: ${(props) => props.theme.backgroundColor};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all .2s;
+`;
+
+export const Button = styled.button`
+  border-style: none;
+  background: #006eed;
+  padding: 1rem 1.6rem;
+  color: #fff;
+  font-weight: 600;
+  border-radius: 10px;
+  box-shadow: 1px 2px 10px 0px rgba(0,0,0,0.51);
+  transition: all .2s;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 1px 2px 10px 0px rgba(0,0,0,0.71);
+  }
+`;
